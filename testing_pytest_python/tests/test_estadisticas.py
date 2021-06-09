15d65e6fe4aba6a5eda900c754be6e01f480dc03
VictoriaBaral <62349218+VictoriaBaral@users.noreply.github.com>
2021-06-09 19:27:44 -0300

    agrego test de estadisticas

--- testing_pytest_python/tests/test_estadisticas.py
+++ testing_pytest_python/tests/test_estadisticas.py
@@ -1,1 +1,23 @@
-from ruleta import Estadisticas 
+from ..ruleta import Estadisticas 
+
+def test_estadisticas():
+
+    ESTADISTICA_PRUEBA = 'test'
+
+    e1 = Estadisticas()
+    assert e1.obtener(ESTADISTICA_PRUEBA) == 0
+    e1.incrementar(ESTADISTICA_PRUEBA)
+    assert e1.obtener(ESTADISTICA_PRUEBA) == 1
+    e1.incrementar(ESTADISTICA_PRUEBA)
+    assert e1.obtener(ESTADISTICA_PRUEBA) == 2
+    e1.incrementar(ESTADISTICA_PRUEBA, 3)
+    assert e1.obtener(ESTADISTICA_PRUEBA) == 5
+    e1.reiniciar()
+    assert e1.obtener(ESTADISTICA_PRUEBA) == 0
+    e1.incrementar(ESTADISTICA_PRUEBA, -1)
+    assert e1.obtener(ESTADISTICA_PRUEBA) == 0
+    e1.incrementar(ESTADISTICA_PRUEBA, 4) 
+    assert e1.obtener(ESTADISTICA_PRUEBA) == 4
+    e1.incrementar(ESTADISTICA_PRUEBA, 0)
+    assert e1.obtener(ESTADISTICA_PRUEBA) == 4
+
