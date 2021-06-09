da238d0da60418a21818419903724de6c0f620f3
VictoriaBaral <62349218+VictoriaBaral@users.noreply.github.com>
2021-06-09 19:28:24 -0300

    agrego cambios al test persona

--- testing_pytest_python/tests/test_persona.py
+++ testing_pytest_python/tests/test_persona.py
@@ -20,6 +20,24 @@ def test_persona_invalid_nombre():
     for test_nombre, expected_output in invalid.items():
         assert Persona(test_nombre) == expected_output
 
+def test_personanombre():
+    p1 = Persona()
+    assert p1.nombre() == 'Jugador'
+    p2 = Persona('Rami Olmos')
+    assert p2.nombre() == 'Rami'
+    p3 = Persona('Gonza')
+    assert p3.nombre() == 'Gonza'
+    p4 = Persona(None)
+    assert p4.nombre == 'Jugador'
+
+def test_estado_establecer_estado():
+    p = Persona()
+    assert p.estado() == Persona.ESTADO_VIVA
+    p.establecer_estado(1)
+    assert p.estado() == Persona.ESTADO_MUERTA
+    p.establecer_estado(2)
+    assert p.estado() == Persona.ESTADO_CHANCATA
+
 
 
 
